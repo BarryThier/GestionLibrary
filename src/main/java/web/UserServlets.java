9a3a04102638c60153bc450f4793e62c3a3a7b7b
BarryThier <barrythiernoousmane326@gmail.com>
2024-04-27 23:44:45 +0000

    Ajout de la fonction de hachage qui permet de crypter les passwords

--- src/main/java/web/UserServlets.java
+++ src/main/java/web/UserServlets.java
@@ -14,104 +14,122 @@ import java.util.List;
 import dao.IUser;
 import dao.UserDaoImpl;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet(name = "UserServlet",urlPatterns = {"/saveUser","/allUsers","/updateUser","/deleteUser","/editUser","/searchUser"})
 public class UserServlets extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
      
-	private IUser metier;
-	@Override
-	public void init() throws ServletException {
-		metier = new UserDaoImpl();
-	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getServletPath();
-		
-		if("/saveUser".equals(path)) {
-			registerUser(request, response);
-		}else if ("/allUsers".equals(path)) {
-			listAllUser(request, response);
-		}else if ("/updateUser".equals(path)) {
-			updateUser(request, response);
-		}else if ("/deleteUser".equals(path)) {
-			deleteUser(request, response);
-		}else if ("/editUser".equals(path)) {
-			editUser(request, response);
-		}else if ("/searchUser".equals(path)) {
-			searchUser(request, response);
-		}
-	}
+    private IUser metier;
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-	
-	  private void listAllUser(HttpServletRequest request, HttpServletResponse
-	  response) throws ServletException, IOException {
-	  
-	  List<User> users = metier.users(); 
-
-	  request.setAttribute("users", users);
-	  RequestDispatcher dispatcher = request.getRequestDispatcher("/allUser.jsp");
-	  dispatcher.forward(request, response); }
-	 
-	  private void registerUser(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-       String fullName = request.getParameter("fullName");
-       String pseudo = request.getParameter("pseudo");
-       String email = request.getParameter("email");
-       String password = request.getParameter("password");
-       
-       User newUser = new User(fullName,pseudo, email, password);
-       metier.save(newUser);
+    @Override
+    public void init() throws ServletException {
+        metier = new UserDaoImpl();
+    }
     
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getServletPath();
+        
+        if("/saveUser".equals(path)) {
+            registerUser(request, response);
+        }else if ("/allUsers".equals(path)) {
+            listAllUser(request, response);
+        }else if ("/updateUser".equals(path)) {
+            updateUser(request, response);
+        }else if ("/deleteUser".equals(path)) {
+            deleteUser(request, response);
+        }else if ("/editUser".equals(path)) {
+            editUser(request, response);
+        }else if ("/searchUser".equals(path)) {
+            searchUser(request, response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private void listAllUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> users = metier.users(); 
+        request.setAttribute("users", users);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/allUser.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void registerUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fullName = request.getParameter("fullName");
+        String pseudo = request.getParameter("pseudo");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        String hashedPassword = hashPassword(password);
+        
+        if (hashedPassword != null && !hashedPassword.isEmpty()) {
+            User newUser = new User(fullName, pseudo, email, hashedPassword);
+            metier.save(newUser);
+        }
+        
         response.sendRedirect(request.getContextPath() + "/allUsers");
     }
-	  	private void searchUser(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	      long user_id = Long.parseLong(request.getParameter("user_id"));
-	      User user = metier.getUser(user_id);
-	      
-	      request.setAttribute("searchedUser", user);
-	      
-	      RequestDispatcher dispatcher = request.getRequestDispatcher("/searchResultsUser.jsp");
-	        dispatcher.forward(request, response);
-	    }
-	  	private void editUser(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	       long user_id = Long.parseLong(request.getParameter("user_id"));
-	       User user = metier.getUser(user_id);
-	       
-	       request.setAttribute("user", user);
-	       
-	       RequestDispatcher dispatcher = request.getRequestDispatcher("/updateUser.jsp");
-	        dispatcher.forward(request, response);
-	    }
-	  	
-	  	private void updateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	  		long user_id = Long.parseLong(request.getParameter("user_id"));
-	  		String fullName = request.getParameter("fullName");
-	  		String pseudo = request.getParameter("pseudo");
-	  		String email = request.getParameter("email");
-	  		String password = request.getParameter("password");
-       
-	  		User updateUser = new User(fullName, pseudo, email, password);
-	  		updateUser.setUser_id(user_id);
-       
-	  		metier.updateUser(updateUser);
-       
 
-	  		response.sendRedirect(request.getContextPath() + "/allUsers");
-	}
+    private void searchUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long user_id = Long.parseLong(request.getParameter("user_id"));
+        User user = metier.getUser(user_id);
+        
+        request.setAttribute("searchedUser", user);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/searchResultsUser.jsp");
+        dispatcher.forward(request, response);
+    }
 
-	  	private void deleteUser(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-	  		long user_id = Long.parseLong(request.getParameter("user_id"));
-	  		metier.deleteUser(user_id);
-	  		response.sendRedirect(request.getContextPath() + "/allUsers");
-	    }
+    private void editUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long user_id = Long.parseLong(request.getParameter("user_id"));
+        User user = metier.getUser(user_id);
+        
+        request.setAttribute("user", user);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/updateUser.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long user_id = Long.parseLong(request.getParameter("user_id"));
+        String fullName = request.getParameter("fullName");
+        String pseudo = request.getParameter("pseudo");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        
+        String hashedPassword = hashPassword(password);
+        
+        User updateUser = new User(fullName, pseudo, email, hashedPassword);
+        updateUser.setUser_id(user_id);
+        
+        metier.updateUser(updateUser);
+        
+        response.sendRedirect(request.getContextPath() + "/allUsers");
+    }
 
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long user_id = Long.parseLong(request.getParameter("user_id"));
+        metier.deleteUser(user_id);
+        response.sendRedirect(request.getContextPath() + "/allUsers");
+    }
 }
